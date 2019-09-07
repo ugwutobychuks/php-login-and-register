d3e258356137e44f0470ee80ca21a4831a12ea63
ugwutobychuks@gmail.com <ugwutobychuks@gmail.com>
2019-09-07 21:21:17 +0100

    first commit

---
+++ testapp.sql
@@ -0,0 +1,38 @@
+/*
+SQLyog Ultimate v11.5 (64 bit)
+MySQL - 5.6.25 : Database - testapp
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`testapp` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `testapp`;
+
+/*Table structure for table `tbl_users` */
+
+DROP TABLE IF EXISTS `tbl_users`;
+
+CREATE TABLE `tbl_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+/*Data for the table `tbl_users` */
+
+insert  into `tbl_users`(`id`,`username`,`email`,`password`) values (1,'dara','dara@gmail.com','e10adc3949ba59abbe56e057f20f883e'),(2,'borey','borey@gmail.com','e10adc3949ba59abbe56e057f20f883e'),(3,'syha','syha@gmail.com','e10adc3949ba59abbe56e057f20f883e');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
